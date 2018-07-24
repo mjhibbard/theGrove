4585c1f0a4a7c754db67f07eaaa96afa13efb57d
Michael Hibbard <38772539+mjhibbard@users.noreply.github.com>
2018-07-24 14:54:23 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser");
+
+
+//App Config
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
+
+app.get("/", function (req, res){
+    res.render("index");
+});
+
+
+
+app.listen(3000, function(){
+    console.log("The Grove server is Listening!!");
+});
+
+//nodemon index.js
