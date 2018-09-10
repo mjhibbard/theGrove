dbb49ded0cb5573dd85d7e9560f0ec8e8e2452bc
Michael Hibbard <38772539+mjhibbard@users.noreply.github.com>
2018-09-09 19:24:24 -0700

    config for promises and set up slash construction page

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser");
+const express       = require("express");
+const path          = require("path");
+const PORT          = 3005;
+// const app         = express();
+// const bodyParser  = require("body-parser");
 
 
-//App Config
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/views'));
+express()
+    //App Config
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    //Routes
+    .get('/', (req, res) => res.render('splash'))
+    .get('/about', (req, res) => res.redirect('/'))
+    .get('/coffee', (req, res) => res.redirect('/'))
 
-app.get("/", function (req, res){
-    res.render("index");
-});
+    .listen(PORT, (req, res) => console.log("The Grove server is Listening!!"))
 
-app.get("/about", function(req, res){
-    res.render("about");
-});
 
-app.get("/coffee", function(req, res){
-    res.render("coffee")
-});
 
-app.get("/showcase", function(req, res){
-    res.render("showcase");
-});
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.set("view engine", "ejs");
+// app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/views'));
 
-app.listen(3005, function(){
-    console.log("The Grove server is Listening!!");
-});
+// app.get("/", function (req, res){
+//     res.render("index");
+// });
+
+// app.get("/about", function(req, res){
+//     res.render("about");
+// });
+
+// app.get("/coffee", function(req, res){
+//     res.render("coffee")
+// });
+
+// app.get("/showcase", function(req, res){
+//     res.render("showcase");
+// });
+
+// app.listen(3005, function(){
+//     console.log("The Grove server is Listening!!");
+// });
 
 //indygrove.church is site and through godaddy.com
 //nodemon index.js
