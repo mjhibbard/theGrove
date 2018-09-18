f06ed202f788f49db2908561931106610e2fe834
Michael Hibbard <38772539+mjhibbard@users.noreply.github.com>
2018-09-18 13:55:11 -0700

    add port const, try for deploy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express       = require("express");
 const path          = require("path");
-const PORT          = 3005;
+const PORT          = process.env.PORT || 3005;
 // const app         = express();
 // const bodyParser  = require("body-parser");
 
